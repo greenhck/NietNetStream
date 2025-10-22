f463cb7841c5db98ca96110d12642848fdd4fe9c
greenhck <24862486gill@gmail.com>
2025-10-22 15:12:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const APP_CONFIG = {
+    tabs: [
+        { id: 'jiostar', name: 'JioStar', type: 'm3u', url: 'data/jioStar.m3u' },
+        { id: 'zee5', name: 'Zee5', type: 'json', url: 'data/zee5.json' },
+        { id: 'movies', name: 'Movies', type: 'json', url: 'data/movies.json' },
+        { id: 'fancode', name: 'Fancode', type: 'json', url: 'https://raw.githubusercontent.com/Odfinity/live-events/refs/heads/main/fancode/data.json' },
+        { id: 'sony', name: 'Sony Network', type: 'm3u', url: 'data/sony-network.m3u' }
+    ]
+};
